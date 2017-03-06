b9a66165d1fd6ce49cf63fbe2901450dc570600d
Stephane Thiell <sthiell@stanford.edu>
2017-03-06 11:48:48 -0800

    customize build.sh to create grafana-oak images

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
 	docker build \
 		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
-		--tag "grafana/grafana:${_grafana_version}" \
+		--tag "grafana-oak:${_grafana_version}" \
 		--no-cache=true .
 	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
 
@@ -14,6 +14,6 @@ else
 	echo "Building latest for master"
 	docker build \
 		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/master/grafana_latest_amd64.deb \
-		--tag "grafana/grafana:master" \
+		--tag "grafana-oak:master" \
 		--no-cache=true .
 fi
